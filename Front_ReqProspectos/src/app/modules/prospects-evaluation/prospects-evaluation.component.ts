0af62e4147babb9363ed19d778cf10a7bc8d85f5
UrbinaArmando <urbinaa01@gmail.com>
2023-05-22 22:48:09 -0700

    first commit

---
+++ Front_ReqProspectos/src/app/modules/prospects-evaluation/prospects-evaluation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prospects-evaluation',
+  templateUrl: './prospects-evaluation.component.html',
+  styleUrls: ['./prospects-evaluation.component.css']
+})
+export class ProspectsEvaluationComponent {
+
+}
---
+++ Front_ReqProspectos/src/app/modules/prospects-list/prospects-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prospects-list',
+  templateUrl: './prospects-list.component.html',
+  styleUrls: ['./prospects-list.component.css']
+})
+export class ProspectsListComponent {
+
+}
